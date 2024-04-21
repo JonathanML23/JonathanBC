17a8884f72d7c29837996dd863dbb46781ddbf0a
JonathanML2311 <jonathandavidm2311@gmail.com>
2024-04-21 16:49:00 -0600

    Agregar archivos de proyecto.

---
+++ DataAccess/ACME/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Globalization;
+
+namespace DataAccess.ACME
+{
+    public class Conexion
+    {
+        private readonly string? _cadenaConexion;
+
+        public Conexion()
+        {
+            string? cadenaConexion;
+
+            //Obtener la cadena de conexion desde variable de entorno
+            cadenaConexion = Environment.GetEnvironmentVariable("SQLServerXE");
+
+            _cadenaConexion = cadenaConexion;
+        }
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection sqlConn;
+
+            //Instancia la conexion utilizando la cadena de conexion obtenida
+            sqlConn = new SqlConnection(_cadenaConexion);
+
+            return sqlConn;
+        }
+    }
+}
---
+++ Models/ACME/EmpresaEntidad.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Models.ACME
+{
+    internal class EmpresaEntidad
+    {
+        [Range (0, int.MaxValue, ErrorMessage = "Debe seleccionar una empresal")]
+        [Display(Name = "Codigo")]
+        public int IDEmpresa { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public int IDTipoEmpresa { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public string Empresa { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public string Direccion { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public string RUC { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public decimal Presupuesto { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un tipo de empresa")]
+        [Display(Name = "Tipo empresa")]
+        public bool Activo { get; set; } = true;
+
+        //Propiedad de navegacion a TipoEmpresa
+        public TipoEmpresaEntidad? TipoEmpresaEntidad { get; set; }
+    }
+}
